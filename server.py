bbdb19a62172d0a1d99068405d51c3a2dbf80ff7
Erez Hajaj <erezhajaj@csil.cs.ucsb.edu>
2019-06-02 16:18:01 -0700

    improved network structure

--- server.py
+++ server.py
@@ -15,17 +15,27 @@ host = "127.0.0.1"
 
 
 #config
-server_id = int(sys.argv[1])
+server_id = sys.argv[1]
 
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 8000 + server_id
+port = config.config[server_id]["server_port"]
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversocket.bind((host, port))
 serversocket.listen(5)
-clientsocket, addr = serversocket.accept()
-print("CLIENT CONNECTED")
 
+sockets = dict()
+
+def connections():
+    while True:
+        clientsocket, addr = serversocket.accept()
+        print("CLIENT CONNECTED")
+
+        msg = clientsocket.recv(1024)
+        socket_name = msg.decode('ascii')
+        sockets[socket_name] = clientsocket
+
+start_new_thread(connections, () )
 
 while True:
     msg = clientsocket.recv(1024)
